bc2bc82dc5f7b12f045ee08e4225d977e5788801
arata <arata04962@gmail.com>
2022-01-06 23:39:32 +0900

    feat: implement console

--- kernel/Main.cpp
+++ kernel/Main.cpp
@@ -1,3 +1,4 @@
+#include "console.hpp"
 #include "fonts.hpp"
 #include "graphics.hpp"
 #include <cstdint>
@@ -11,6 +12,9 @@ void operator delete(void *obj) noexcept {}
 char pixel_writer_buf[sizeof(RGBResv8BitPixelWriter)];
 PixelWriter *pixel_writer;
 
+char console_buf[sizeof(Console)];
+Console *console;
+
 extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config) {
   switch (frame_buffer_config.pixel_format) {
   case kPixelRGBResv8BitPerColor:
@@ -21,21 +25,11 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config) {
     break;
   }
 
-  for (int x = 0; x < frame_buffer_config.horizontal_resolution; ++x) {
-    for (int y = 0; y < frame_buffer_config.vertical_resolution; ++y) {
-      pixel_writer->Write(x, y, {255, 255, 255});
-    }
-  }
-  for (int x = 0; x < 200; ++x) {
-    for (int y = 0; y < 100; ++y) {
-      pixel_writer->Write(x, y, {0, 255, 0});
-    }
-  }
+  console = new (console_buf) Console(*pixel_writer, {0, 0, 0}, {255, 255, 255});
 
-  for (char c = '!'; c <= '~'; ++c) {
-    WriteAscii(*pixel_writer, 8 * (c - '!'), 50, c, {0, 0, 0});
+  while (true) {
+    console->PutString("Test\n");
   }
-  WriteString(*pixel_writer, 0, 66, "Hello, world!", {0, 0, 255});
 
   while (1)
     __asm__("hlt");
