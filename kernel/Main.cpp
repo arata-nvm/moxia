424b662306a538ae246c85439f076383d84bd3c3
arata <arata04962@gmail.com>
2022-01-06 23:45:22 +0900

    feat: implement printk

--- kernel/Main.cpp
+++ kernel/Main.cpp
@@ -2,18 +2,38 @@
 #include "fonts.hpp"
 #include "graphics.hpp"
 #include <cstdint>
+#include <stdarg.h>
 #include <stddef.h>
+#include <stdio.h>
+#include <sys/types.h>
 
 void *operator new(size_t size, void *buf) {
   return buf;
 }
 void operator delete(void *obj) noexcept {}
 
+extern "C" caddr_t sbrk(int incr) {
+  return NULL;
+}
+
 char pixel_writer_buf[sizeof(RGBResv8BitPixelWriter)];
 PixelWriter *pixel_writer;
 
 char console_buf[sizeof(Console)];
 Console *console;
+
+int printk(const char *format...) {
+  va_list ap;
+  int result;
+  char s[1024];
+
+  va_start(ap, format);
+  result = vsprintf(s, format, ap);
+  va_end(ap);
+
+  console->PutString(s);
+  return result;
+}
 
 extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config) {
   switch (frame_buffer_config.pixel_format) {
@@ -27,8 +47,8 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config) {
 
   console = new (console_buf) Console(*pixel_writer, {0, 0, 0}, {255, 255, 255});
 
-  while (true) {
-    console->PutString("Test\n");
+  for (int i = 0; i < 27; ++i) {
+    printk("printk: %d\n", i);
   }
 
   while (1)
