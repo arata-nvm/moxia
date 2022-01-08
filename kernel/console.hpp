e9f90db10470bb91281be58754a018f41311456a
arata <arata04962@gmail.com>
2022-01-08 15:26:55 +0900

    fix: set text color correctly

--- kernel/console.hpp
+++ kernel/console.hpp
@@ -13,7 +13,7 @@ private:
   void NewLine();
 
   PixelWriter &writer_;
-  const PixelColor &fg_color_, bg_color_;
+  const PixelColor fg_color_, bg_color_;
   char buffer_[kRows][kColumns + 1];
   int cursor_row_, cursor_column_;
 };
