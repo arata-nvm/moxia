c20785de9cadad3dc4e873adc787090dd7fd8b39
arata <arata04962@gmail.com>
2022-01-27 00:37:11 +0900

    feat: implement __cxa_pure_virtual

--- kernel/libcxx_support.cpp
+++ kernel/libcxx_support.cpp
@@ -12,3 +12,8 @@ std::new_handler std::get_new_handler() noexcept {
 extern "C" int posix_memalign(void **, size_t, size_t) {
   return ENOMEM;
 }
+
+extern "C" void __cxa_pure_virtual() {
+  while (1)
+    ;
+}
