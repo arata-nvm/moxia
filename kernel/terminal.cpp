1e2676d4efa847f32e4b64220f4536f6600b416f
arata <arata04962@gmail.com>
2022-02-20 23:56:09 +0900

    feat: add ls command

--- kernel/terminal.cpp
+++ kernel/terminal.cpp
@@ -1,5 +1,6 @@
 #include "terminal.hpp"
 #include "console.hpp"
+#include "fat.hpp"
 #include "keyboard.hpp"
 #include "printk.hpp"
 #include "task.hpp"
@@ -19,6 +20,28 @@ void ExecuteCommand(std::string line) {
     printk("%s\n", arg.c_str());
   } else if (cmd == "clear") {
     console->Clear();
+  } else if (cmd == "ls") {
+    fat::DirectoryEntry *root_dir_entries = fat::GetSectorByCluster<fat::DirectoryEntry>(fat::boot_volume_image->root_cluster);
+    int entries_per_cluster = fat::boot_volume_image->bytes_per_sector / sizeof(fat::DirectoryEntry) * fat::boot_volume_image->sectors_per_cluster;
+    char base[9], ext[4];
+    char s[64];
+    for (int i = 0; i < entries_per_cluster; ++i) {
+      fat::ReadName(root_dir_entries[i], base, ext);
+      if (base[0] == 0x00) {
+        break;
+      } else if (static_cast<uint8_t>(base[0]) == 0xe5) {
+        continue;
+      } else if (root_dir_entries[i].attr == fat::Attribute::kLongName) {
+        continue;
+      }
+
+      if (ext[0]) {
+        sprintf(s, "%s.%s\n", base, ext);
+      } else {
+        sprintf(s, "%s\n", base);
+      }
+      printk("%s", s);
+    }
   } else {
     printk("command not found: %s\n", cmd.c_str());
   }
