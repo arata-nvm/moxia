e4059d6011acb428599876e5635c143e80c09268
arata <arata04962@gmail.com>
2022-03-26 16:55:40 +0900

    feat: add fault handlers

--- kernel/interrupt.cpp
+++ kernel/interrupt.cpp
@@ -8,9 +8,52 @@
 #include <array>
 
 std::array<InterruptDescriptor, 256> idt;
-
 namespace {
-__attribute__((interrupt)) void IntHandlerKeyboard(InterruptFrame *frame) {
+void PrintFrame(InterruptFrame *frame, const char *exp_name) {
+  printk("%s\n", exp_name);
+  printk("CS: %x\n", frame->cs);
+  printk("RIP: %x\n", frame->rip);
+  printk("RFLAGS: %x\n", frame->rflags);
+  printk("SS: %x\n", frame->ss);
+  printk("RSP: %x\n", frame->rsp);
+}
+
+#define FaultHandlerWithError(fault_name)                                                              \
+  __attribute__((interrupt)) void IntHandler##fault_name(InterruptFrame *frame, uint64_t error_code) { \
+    PrintFrame(frame, "#" #fault_name);                                                                \
+    printk("[ERR] %x\n", error_code);                                                                  \
+    while (true)                                                                                       \
+      __asm__("hlt");                                                                                  \
+  }
+
+#define FaultHandlerWithNoError(fault_name)                                       \
+  __attribute__((interrupt)) void IntHandler##fault_name(InterruptFrame *frame) { \
+    PrintFrame(frame, "#" #fault_name);                                           \
+    while (true)                                                                  \
+      __asm__("hlt");                                                             \
+  }
+
+FaultHandlerWithNoError(DE);
+FaultHandlerWithNoError(DB);
+FaultHandlerWithNoError(DP);
+FaultHandlerWithNoError(OF);
+FaultHandlerWithNoError(BR);
+FaultHandlerWithNoError(UD);
+FaultHandlerWithNoError(NM);
+FaultHandlerWithError(DF);
+FaultHandlerWithError(TS);
+FaultHandlerWithError(NP);
+FaultHandlerWithError(SS);
+FaultHandlerWithError(GP);
+FaultHandlerWithError(PF);
+FaultHandlerWithNoError(MF);
+FaultHandlerWithError(AC);
+FaultHandlerWithNoError(MC);
+FaultHandlerWithNoError(XM);
+FaultHandlerWithNoError(VE);
+
+__attribute__((interrupt)) void
+IntHandlerKeyboard(InterruptFrame *frame) {
   KeyboardOnInterrupt();
 }
 } // namespace
@@ -41,7 +84,31 @@ void InitializeInterrupt() {
   DisablePIC();
   PICClearMask(InterruptVector::kKeyboard);
 
-  SetIDTEntry(idt[InterruptVector::kKeyboard], MakeIDTAttr(DescriptorType::kInterruptGate, 0), reinterpret_cast<uint64_t>(IntHandlerKeyboard), kKernelCS);
-  SetIDTEntry(idt[InterruptVector::kLAPICTimer], MakeIDTAttr(DescriptorType::kInterruptGate, 0), reinterpret_cast<uint64_t>(IntHandlerLAPICTimer), kKernelCS);
+  auto set_idt_entry = [](int irq, auto handler) {
+    SetIDTEntry(idt[irq], MakeIDTAttr(DescriptorType::kInterruptGate, 0), reinterpret_cast<uint64_t>(handler), kKernelCS);
+  };
+
+  set_idt_entry(InterruptVector::kKeyboard, IntHandlerKeyboard);
+  set_idt_entry(InterruptVector::kLAPICTimer, IntHandlerLAPICTimer);
+
+  set_idt_entry(0, IntHandlerDE);
+  set_idt_entry(1, IntHandlerDB);
+  set_idt_entry(3, IntHandlerDP);
+  set_idt_entry(4, IntHandlerOF);
+  set_idt_entry(5, IntHandlerBR);
+  set_idt_entry(6, IntHandlerUD);
+  set_idt_entry(7, IntHandlerNM);
+  set_idt_entry(8, IntHandlerDF);
+  set_idt_entry(10, IntHandlerTS);
+  set_idt_entry(11, IntHandlerNP);
+  set_idt_entry(13, IntHandlerSS);
+  set_idt_entry(14, IntHandlerGP);
+  set_idt_entry(15, IntHandlerPF);
+  set_idt_entry(16, IntHandlerMF);
+  set_idt_entry(17, IntHandlerAC);
+  set_idt_entry(18, IntHandlerMC);
+  set_idt_entry(19, IntHandlerXM);
+  set_idt_entry(20, IntHandlerVE);
+
   LoadIDT(sizeof(idt) - 1, reinterpret_cast<uintptr_t>(&idt[0]));
 }
